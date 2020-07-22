f835e8e3c5b0dc5f5d7e57f7d0341f9647755161
xiaolin <462441355@qq.com>
2020-07-22 18:01:01 +0800

    * 增加注释

--- extend/index.js
+++ extend/index.js
@@ -1,10 +1,14 @@
 import mpExtend from './mp-extend.js'
 
 import extendPage from './complex/extend-page.js'
+import extendGlobalData from './complex/extend-globalData.js'
 
 import simplePv from './simple/extend_pv.js'
 
+// 以mpExtend(...)的形式引入多个扩展
+// 如果不需要某一个混入项的功能，直接注释掉即可
 mpExtend(extendPage)
+mpExtend(extendGlobalData)
 
 mpExtend(simplePv)
 
