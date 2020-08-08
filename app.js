188b35d7c5ee42e07d0d2fd74b49adf1233578a7
xiaolin <462441355@qq.com>
2020-08-08 15:12:32 +0800

    * 增加测试globalSetData

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ Component = extend.Component
 
 App({
   onLaunch: function () {
-   
+    // 在app.js中使用 $globalSetData：
+    // 如果是同步的，可以直接赋值给当前的globalData
+    this.globalData.testData = 'hello';
+
+    // 同步和异步的都可以使用 $globalSetData
+    this.$globalSetData({
+      testData2: "testData2"
+    })
+    setTimeout(() => {
+      this.$globalSetData({
+        testData: "tick1"
+      })
+    }, 5000)
   },
-  globalData: {
-  }
+  globalData: {}
 })
